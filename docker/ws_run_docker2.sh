b4246fc8f9cc13fc3b83076993ac4fa4d552546d
Hanzhen Zhang <hanzhen.zhang@tum.de>
2022-05-05 10:48:29 +0200

    update docker container

---
+++ docker/ws_run_docker2.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Port bindings: -p host_port:container_port
+# Port bindings: -p host_port:container_port
+read -p "Tensorboard port (host) "  TboardHostPort
+read -p "Jupyter Notebook port (host) "  JupyHostPort
+# Path where the KITTI Odometry dataset files are kept or to be saved
+KITTI_PATH="/mount/HDD2/Datasets/Odometry"
+#Path where ModelNet40 are saved
+MODELNET40_PATH="/home/hanzhen/ModelNet40"
+#Path where the trained models are saved
+MODEL_PATH="/home/hanzhen/models"
+#Path where the deepclr model are saved
+Deepclr="/home/hanzhen/VO_git/deepclr"
+#'"device=1"'
+docker run -it --gpus "device=0" -v $KITTI_PATH:/KITTI_PATH \
+				-v $MODELNET40_PATH:/MODELNET40_PATH \
+				-v $Deepclr:/code \
+				-v $MODEL_PATH:/MODEL_PATH \
+				-p $JupyHostPort:8888 \
+				-p $TboardHostPort:6006 \
+				ghcr.io/mhorn11/deepclr/deepclr-deps:1.0
+cd ..
+export KITTI_PATH=/KITTI_PATH
+export MODELNET40_PATH=/MODELNET40_PATH
+
